9301608ca83db61610d1522445a005dd05c44030
supriya <supriya@localhost.localdomain>
2015-09-03 17:11:34 +0530

    Changed the php file

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
 // Show all information, defaults to INFO_ALL
-phpinfo();
+<?php
+print "<h2>Hello from JBoss Developer Studio!</h2>";
+print "This is OpenShift v3!<br>";
+print "Have fun!";
+?>();
 
 ?>
